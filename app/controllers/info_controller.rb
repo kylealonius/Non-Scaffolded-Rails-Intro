fb44a319c053333295fabb7d8588336c070ab2fd
Kyle Alonius <k_alonius@hotmail.com>
2013-10-28 13:26:17 -0500

    Added an info controller with three actions and associated views

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,16 @@
+class InfoController < ApplicationController
+  #technically, none of these actions are actually required, because they contain no code
+  #However, they do serve as documentation for our project
+  
+  def index   
+  end
+  #Loads the app/views/info/index.html.erb
+  
+  def contact
+  end 
+  #Loads the app/views/info/contact.html.erb
+  
+  def about
+  end
+  #Loads the app/views/info/about.html.erb
+end
